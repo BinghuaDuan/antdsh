d470cf825fedab11277cd1d762fc3df1081451cb
beim <805908499@qq.com>
2019-05-31 17:49:39 +0800

    修改服务资源库入口（将keywordsearch页面和模板页面放在一起）

--- src/schema/page/SchemaMainPage.js
+++ src/schema/page/SchemaMainPage.js
@@ -14,30 +14,37 @@ class SchemaMainPage extends Component {
     const listSchemaCardData = {
       href: SCHEMA_CONST.HREF.LIST,
       imgSrc: "/images/menu.png",
-      title: "列表",
+      title: "模板列表",
       desc: "查看服务资源模板列表",
     };
     const newSchemaCardData = {
       href: SCHEMA_CONST.HREF.NEW,
       imgSrc: "/images/plus.png",
-      title: "新建",
+      title: "新建模板",
       desc: "新建服务资源模板",
+    };
+    const browserCardData = {
+      href: '/demo/keywordsearch',
+      title: "服务资源库",
+      desc: "检索服务资源库",
     };
     return (
       <Layout className="layout" style={{ height: "100%" }}>
         <MenuHeader defaultSelectedKey="2" />
         <Content style={{ padding: '0 50px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>模板</Breadcrumb.Item>
+            <Breadcrumb.Item>首页</Breadcrumb.Item>
+            <Breadcrumb.Item>面向跨界异构服务融合的服务资源库</Breadcrumb.Item>
           </Breadcrumb>
           <div className={commonStyles.pageBackground}>
             <div className={commonStyles.page}>
               <div className={commonStyles.content}>
                 <div className={commonStyles.card}>
-                  <div className={commonStyles.cardTitle}>服务资源模板</div>
+                  <div className={commonStyles.cardTitle}>面向跨界异构服务融合的服务资源库</div>
                   <div>
                     <CommonProductCard data={listSchemaCardData} />
                     <CommonProductCard data={newSchemaCardData} />
+                    <CommonProductCard data={browserCardData} />
                   </div>
                 </div>
               </div>
